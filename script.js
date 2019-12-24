a445eb5dcb64fa108be9614e804460e504cc10c4
her_username <emailfromhimanshu@gmail.com>
2019-12-24 23:57:39 +0530

    products api integrated

--- script.js
+++ script.js
@@ -1,15 +1,33 @@
 $(document).ready(function() {
-  console.log("text");
   const template = Handlebars.compile($("#category").html());
-  console.log();
+  const productTemplate = Handlebars.compile($("#products").html());
   $.ajax({
     url:
       "http://staging.lazysuzy.com/api/products/all?filters=brand:pier1;&sort_type=&pageno=0",
     context: document.body
   }).done(function(data) {
-    console.log(data);
     $(".components")
       .children(".row")
       .append(template({ categories: data.filterData.category }));
+  });
+
+  $("body").on("click", ".category-label", function() {
+    const category = $(this).attr("data-category");
+    $.ajax({
+      url: `http://staging.lazysuzy.com/api/products/all?filters=brand:pier1;category:${category}&sort_type=&pageno=0`,
+      context: document.body
+    }).done(function(data) {
+      $(".products")
+        .children(".row")
+        .html(productTemplate({ products: data.products }));
+    });
+  });
+
+  $(".nav-link").click(function(event) {
+    event.preventDefault();
+    $(".nav-link").removeClass("active");
+    $(this).addClass("active");
+    $(".tab").removeClass("active");
+    $($(this).attr("href")).addClass("active");
   });
 });
