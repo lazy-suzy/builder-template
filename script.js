b021cb101092dbc648bbb824e8a575354f0c4301
her_username <emailfromhimanshu@gmail.com>
2019-12-25 00:29:29 +0530

    bottom panel

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
   const template = Handlebars.compile($("#category").html());
+  let products = [];
   const productTemplate = Handlebars.compile($("#products").html());
+  const bottomPanelTemplate = Handlebars.compile($("#bottom-panel").html());
   $.ajax({
     url:
       "http://staging.lazysuzy.com/api/products/all?filters=brand:pier1;&sort_type=&pageno=0",
@@ -17,9 +19,11 @@ $(document).ready(function() {
       url: `http://staging.lazysuzy.com/api/products/all?filters=brand:pier1;category:${category}&sort_type=&pageno=0`,
       context: document.body
     }).done(function(data) {
+      products = data.products;
       $(".products")
         .children(".row")
         .html(productTemplate({ products: data.products }));
+      $(".bottom-panel").html(bottomPanelTemplate(products[0]));
     });
   });
 
@@ -29,5 +33,10 @@ $(document).ready(function() {
     $(this).addClass("active");
     $(".tab").removeClass("active");
     $($(this).attr("href")).addClass("active");
+  });
+
+  $("body").on("click", ".product-image", function() {
+    const product = products[$(this).attr("data-product")];
+    $(".bottom-panel").html(bottomPanelTemplate(product));
   });
 });
